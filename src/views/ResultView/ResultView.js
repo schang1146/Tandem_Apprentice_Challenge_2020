3f52c5d3061f03be8588dfd5516dd200dc8ae80c
schang1146 <sammychang2185@gmail.com>
2020-10-28 00:12:42 -0700

    trivia game functions and plays

--- src/views/ResultView/ResultView.js
+++ src/views/ResultView/ResultView.js
@@ -1,10 +1,13 @@
 // import dependencies
+import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
 
 function ResultView() {
+    const history = useHistory();
+
     return (
         <div className='result-wrapper'>
-            <h2>You scored 0/10!</h2>
+            <h2>You scored {history.location.state.score}/10!</h2>
             <Link to='/trivia'>
                 <button>Retry</button>
             </Link>
--- src/views/TriviaView/TriviaView.js
+++ src/views/TriviaView/TriviaView.js
@@ -1,28 +1,106 @@
 // import dependencies
+import { useState, useEffect } from 'react';
 import { useHistory } from 'react-router';
 
+// import trivia database
+import data from '../../api/Apprentice_TandemFor400_Data.json';
+
+// function to generate a set of 10 random problems without duplicates
+function generateProblems() {
+    let added = new Set();
+    let problems = [];
+
+    while (problems.length !== 10) {
+        const id = getRandomInt(data.length);
+        const answers = shuffle(data[id].incorrect.concat([data[id].correct]));
+        data[id]['answers'] = answers;
+        added.add(id);
+        problems.push(data[id]);
+    }
+
+    return problems;
+}
+
+// function to get a random integer from 0 -> data.length (exclusive)
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// function to shuffle a given array
+// uses durstenfeld's shuffling algorithm
+function shuffle(array) {
+    for (let i = 0; i < array.length - 1; i++) {
+        const j = Math.floor(Math.random() * array.length);
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
 function TriviaView() {
+    // componentdidmount - generate questions
+    const [problems, setProblems] = useState([]);
+    useEffect(() => {
+        const generatedProblems = generateProblems;
+        setProblems(generatedProblems);
+    }, []);
+    const [score, setScore] = useState(0);
+    const [questionId, setQuestionId] = useState(0);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [isSubmitted, setSubmitted] = useState(false);
+
     const history = useHistory();
+    const next = (e) => {
+        e.preventDefault();
+
+        if (questionId === 9) {
+            history.push({
+                pathname: '/result',
+                state: { score: score },
+            });
+        } else {
+            setQuestionId(questionId + 1);
+        }
+
+        setSubmitted(false);
+        setSelectedAnswer(null);
+    };
+
     const submit = (e) => {
         e.preventDefault();
-        history.push('/result');
+
+        if (problems[questionId].answers.indexOf(problems[questionId].correct) === selectedAnswer) {
+            setScore(score + 1);
+            console.log('correct');
+        } else {
+            console.log('incorrect');
+        }
+
+        setSubmitted(true);
     };
 
     return (
         <div className='trivia-wrapper'>
-            <form className='trivia-form' onSubmit={(e) => submit(e)}>
-                <div className='trivia-score'>Score: 0</div>
-                <div className='trivia-question'>What is a question statement?</div>
-                <div className='trivia-answers'>
-                    <ol type='A'>
-                        <li>Answer 1</li>
-                        <li>Answer 2</li>
-                        <li>Answer 3</li>
-                        <li>Answer 4</li>
-                    </ol>
-                </div>
-                <input type='submit' value='Next' />
-            </form>
+            {problems.length ? (
+                <form className='trivia-form' onSubmit={(e) => submit(e)}>
+                    <div className='trivia-score'>Score: {score}/10</div>
+                    <div className='trivia-problem'>Problem #{questionId + 1}</div>
+                    <div className='trivia-question'>{problems[questionId].question}</div>
+                    <div className='trivia-answers'>
+                        <ol type='A'>
+                            {problems[questionId].answers.map((answer, index) => {
+                                return (
+                                    <li key={index} onClick={() => setSelectedAnswer(index)}>
+                                        {answer}
+                                    </li>
+                                );
+                            })}
+                        </ol>
+                    </div>
+                    {!isSubmitted ? <input type='submit' value='Submit' /> : <button onClick={(e) => next(e)}>Next</button>}
+                </form>
+            ) : (
+                <div>Loading...</div>
+            )}
         </div>
     );
 }
