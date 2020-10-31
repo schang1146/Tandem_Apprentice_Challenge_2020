f3cfb7c828432a5299bcc6e7cfd0a2ad79bfe12e
schang1146 <sammychang2185@gmail.com>
2020-10-30 22:16:01 -0700

    finished with result styling... for now

--- src/views/ResultView/ResultView.js
+++ src/views/ResultView/ResultView.js
@@ -2,17 +2,35 @@
 import { useHistory } from 'react-router';
 import { Link } from 'react-router-dom';
 
+// import styling
+import './ResultView.scss';
+
 function ResultView() {
     const history = useHistory();
-
+    const titles = {
+        '-1': 'How...?',
+        0: 'Trivia Newcomer',
+        1: 'Trivia Novice',
+        2: 'Trivia Beginner',
+        3: 'Trivia Dabbler',
+        4: 'Trivia Amateur',
+        5: 'Trivia Regular',
+        6: 'Trivia Expert',
+        7: 'Trivia Wizard',
+        8: 'Trivia Master',
+        9: 'Trivia Guru',
+        10: 'Trivia Grandmaster',
+        11: 'Cheater!',
+    };
     return (
         <div className='result-wrapper'>
-            <h2>You scored {history.location.state.score}/10!</h2>
-            <Link to='/trivia'>
-                <button>Retry</button>
+            <h2 className='result-score'>You scored {history.location.state.score}/10!</h2>
+            <h3 className='result-title'>Title: {titles[history.location.state.score]}</h3>
+            <Link className='btn-retry' to='/trivia'>
+                Retry
             </Link>
-            <Link to='/'>
-                <button>Back to Home</button>
+            <Link className='btn-home' to='/'>
+                Back to Home
             </Link>
         </div>
     );
