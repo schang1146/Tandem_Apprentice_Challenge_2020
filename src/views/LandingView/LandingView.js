591b75bf4465156786dc41edd021c17648a588ea
Sammy Chang <sammychang2185@gmail.com>
2021-01-29 18:22:25 -0800

    Fix styling for LandingView

--- src/views/LandingView/LandingView.js
+++ src/views/LandingView/LandingView.js
@@ -4,6 +4,9 @@ import { Link } from 'react-router-dom';
 import 'firebase/auth';
 import firebaseApp from '../../firebase';
 
+// import components
+import Layout from '../../components/Layout/Layout';
+
 // import styling
 import './LandingView.scss';
 
@@ -19,20 +22,22 @@ function LandingView() {
     });
 
     return (
-        <div className='landing-wrapper'>
-            <header className='landing-header'>
-                <div className='landing-top'>
-                    {user && `Welcome ${user.displayName}!`}
-                    <h1 className='landing-logo'>tandem for 400!</h1>
-                    <h2 className='landing-title'>Build your Trivia muscles!</h2>
-                </div>
-                <div className='landing-bot'>
-                    <Link className='btn-start' to='/trivia'>
-                        Start
-                    </Link>
-                </div>
-            </header>
-        </div>
+        <Layout style={{ background: `linear-gradient(to bottom, #fff4e6 80%, #ffffff 20%)` }}>
+            <div className='landing-wrapper'>
+                <header className='landing-header'>
+                    <div className='landing-top'>
+                        {user && `Welcome ${user.displayName}!`}
+                        <h1 className='landing-logo'>tandem for 400!</h1>
+                        <h2 className='landing-title'>Build your Trivia muscles!</h2>
+                    </div>
+                    <div className='landing-bot'>
+                        <Link className='btn-start' to='/trivia'>
+                            Start
+                        </Link>
+                    </div>
+                </header>
+            </div>
+        </Layout>
     );
 }
 
